8f34d786185089b8e16076cb4cd733f5a913277b
michelepotuto <michele.potuto1990@gmail.com>
2022-07-05 15:08:29 +0200

    sdvsv

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "./components/Navbar";
 import { Fragment } from "react";
-import Navbar from "./components/Login";
+import Login from "./components/Login";
 
 
 function App() {
